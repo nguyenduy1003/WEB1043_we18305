30a3ab348cd3f7e05241115077da1455e0a6e3b5
huu duy <duynlhpd08257@fpt.edu.vn>
2023-03-09 16:23:59 +0700

    up

---
+++ Lab1/app.js
@@ -0,0 +1,9 @@
+// Nhập vào 2 số nguyên từ bàn phím
+let a = prompt("Nhập số nguyên thứ nhất:");
+let b = prompt("Nhập số nguyên thứ hai:");
+
+// Chuyển đổi chuỗi sang số và tính tổng
+let c = parseInt(a) + parseInt(b);
+// In ra kết quả bằng document.write và console.log
+document.write(`Tổng của ${a} và ${b} là: ${c}`);
+console.log(`Tổng của ${a} và ${b} là: ${c}`);
